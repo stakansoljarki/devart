619c278f5b3bdbb5eee0473d89962ac1dd01dc0d
stakansoljarki <roman.dybach@gmail.com>
2022-07-22 17:21:25 +0300

    Fix error

--- src/app/layouts/Clients/Clients.jsx
+++ src/app/layouts/Clients/Clients.jsx
@@ -3,21 +3,19 @@ import Title from '../../components/Title/Title';
 
 import clientsCardData from './assets/cardConstants';
 
-const Clients = () => {
-  return (
-    <section className="clients">
-      <div className="clients__container">
-        <Title className="clients__title">Leading companies choose dbForge Products</Title>
-        <div className="clients__cards-wrap">
-          {clientsCardData.map((card) => (
-            <div key={card.id} className="clients__card">
-              <img src={card.src} alt={card.alt} className="clients__logo" />
-            </div>
-          ))}
-        </div>
+const Clients = () => (
+  <section className="clients">
+    <div className="clients__container">
+      <Title className="clients__title">Leading companies choose dbForge Products</Title>
+      <div className="clients__cards-wrap">
+        {clientsCardData.map((card) => (
+          <div key={card.id} className="clients__card">
+            <img src={card.src} alt={card.alt} className="clients__logo" />
+          </div>
+        ))}
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 export default Clients;
